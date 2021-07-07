c325cac9344664c6321d816150804c8a800b07ec
840288 <840288@SAIT225697IT.ACDM.DS.SAIT.CA>
2021-07-07 10:46:22 -0600

    Fourth Commit worked on & added code to models class User & Role, also added code to DBUtil, and addded code to context.xml

--- Lab07_JDBC/src/java/models/Role.java
+++ Lab07_JDBC/src/java/models/Role.java
@@ -5,10 +5,38 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 840288
  */
-public class Role {
+public class Role implements Serializable{
+    private int roleId;
+    private String roleName;
+    
+    public Role() {
+    }   
+    
+    public Role(int roleId, String roleName) {
+        roleId = this.roleId;
+        roleName = this.roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
     
 }
--- Lab07_JDBC/src/java/models/User.java
+++ Lab07_JDBC/src/java/models/User.java
@@ -5,10 +5,67 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 840288
  */
-public class User {
+public class User implements Serializable {
+    private String email;
+    private boolean active;
+    private String firstname;
+    private String lastname;
+    private String password;
     
+    public User(){
+    }
+    
+    public User(String email, boolean active, String firstname, String lastname, String password) {
+        email = this.email;
+        active = this.active;
+        firstname = this.firstname;
+        lastname = this.lastname;
+        password = this.password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }  
 }
